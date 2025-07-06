3e7d282a51c47566788ed398eb2fefec01e17376
kevyan24 <yan24010@byui.edu>
2025-07-05 20:18:40 -0500

    feat: nav links

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,5 +1,8 @@
 const navbutton = document.querySelector('#ham-btn');
+const navlinks = document.querySelector('#nav-bar');
 
 navbutton.addEventListener('click', () => {
   navbutton.classList.toggle('show');
+  navlinks.classList.toggle('show');
 });
+
