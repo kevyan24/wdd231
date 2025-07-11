cb6cebbde4d57d7c206150124788848fbd83966a
kevyan24 <yan24010@byui.edu>
2025-07-10 19:22:32 -0500

    fix: errors

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -25,6 +25,8 @@ function displayProphets(prophets) {
     portrait.setAttribute('src', prophet.imageurl);
     portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
     portrait.setAttribute('loading', 'lazy');
+    portrait.setAttribute('width', '340');
+    portrait.setAttribute('height', '440');
 
     card.appendChild(fullName);
     card.appendChild(birthDate);
